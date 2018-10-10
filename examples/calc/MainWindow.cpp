80acbce4b083777692b57e0c0fbacfe1162d8b2c
Sean Farrell <sean.farrell@rioki.org>
2018-10-10 18:03:47 +0200

    Implements example calc.

--- examples/calc/MainWindow.cpp
+++ examples/calc/MainWindow.cpp
@@ -23,6 +23,7 @@
 
 #include "pch.h"
 
+#include <rtk/dbg.h>
 #include "MainWindow.h"
 
 namespace calc
@@ -30,5 +31,164 @@ namespace calc
 	MainWindow::MainWindow()
 	: rtk::Window(CW_USEDEFAULT, CW_USEDEFAULT, 166, 252, "Calculator")
 	{
+        display.set_text("0");
+
+        button_c.on_click([this] () {
+            display.set_text("0");
+        });
+        button_ce.on_click([this] () {
+            value = 0.0f;
+            op = NOP;
+            display.set_text("0");
+        });
+        button_bk.on_click([this] () {
+            auto text = display.get_text();
+            if (text.size() == 1)
+            {
+                display.set_text("0");
+            }
+            else
+            {
+                display.set_text(text.substr(0, text.size() - 1));
+            }
+        });
+
+        button_1.on_click([this] () {
+            add_digit('1');
+        });
+        button_2.on_click([this] () {
+            add_digit('2');
+        });
+        button_3.on_click([this] () {
+            add_digit('3');
+        });
+        button_4.on_click([this] () {
+            add_digit('4');
+        });
+        button_5.on_click([this] () {
+            add_digit('5');
+        });
+        button_6.on_click([this] () {
+            add_digit('6');
+        });
+        button_7.on_click([this] () {
+            add_digit('7');
+        });
+        button_8.on_click([this] () {
+            add_digit('8');
+        });
+        button_9.on_click([this] () {
+            add_digit('9');
+        });
+        button_0.on_click([this] () {
+            add_digit('0');
+        });
+
+        button_dot.on_click([this] () {
+            add_digit('.');
+        });
+        button_neg.on_click([this] () {
+            add_digit('-');
+        });
+        
+        button_add.on_click([this] () {
+            push_op(ADD);
+        });
+        button_sub.on_click([this] () {
+            push_op(SUB);
+        });
+        button_mul.on_click([this] () {
+            push_op(MUL);
+        });
+        button_div.on_click([this] () {
+            push_op(DIV);
+        });
+        button_eva.on_click([this] () {
+            push_op(EVA);
+        });
 	}
+
+    void MainWindow::add_digit(char c) 
+    {
+        auto text = display.get_text();
+
+        if (op == EVA)
+        {
+            op = NOP;
+            text = "0";
+        }
+
+        if (c == '.')
+        {
+            if (text.find('.') == std::string::npos)
+            {
+                text.push_back(c);    
+            }
+        }
+        else if (c == '-')
+        {
+            if (text[0] != '-')
+            {
+                text = "-" + text;
+            }
+        }
+        else
+        {
+            if (text == "0")
+            {
+                text = c;
+            }
+            else if (text == "-0")
+            {
+                text = "-";
+                text.push_back(c);
+            }
+            else
+            {
+                text.push_back(c);  
+            }
+        }
+
+        display.set_text(text);
+    }
+
+    void MainWindow::push_op(OP next_op)
+    {
+        auto text = display.get_text();
+        float v = std::stof(text);
+        
+        switch (op)
+        {
+            case NOP:
+                value = v;
+                break;
+            case ADD:
+                value += v;
+                break;
+            case SUB:
+                value -= v;
+                break;
+            case MUL:
+                value *= v;
+                break;
+            case DIV:
+                value /= v;
+                break;
+            case EVA:
+                // nothing
+                break;
+            default:
+                DBG_FAIL("Unkown OP");
+        }
+
+        op = next_op;
+        if (next_op != EVA)
+        {
+            display.set_text("0");
+        }
+        else
+        {
+            display.set_text(std::to_string(value));
+        }
+    }
 }
