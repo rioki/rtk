7d22408a02f9d95c70733a07feb821bedfa5f87f
Sean Farrell <sean.farrell@rioki.org>
2018-11-10 18:05:39 +0100

    Build proper menu structure

--- examples/notes/MainWindow.cpp
+++ examples/notes/MainWindow.cpp
@@ -3,21 +3,21 @@
 // Copyright 2018 Sean "rioki" Farrell <sean.farrell@rioki.org>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to 
-// deal in the Software without restriction, including without limitation the 
-// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
-// sell copies of the Software, and to permit persons to whom the Software is 
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
 // The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE 
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 // DEALINGS IN THE SOFTWARE.
 //
 
@@ -30,7 +30,7 @@ namespace notes
     MainWindow::MainWindow()
     : rtk::Window(L"notes")
     {
-        create_menu();    
+        create_menu();
     }
 
     void MainWindow::create_menu()
@@ -39,17 +39,13 @@ namespace notes
 
         auto fileMenu = std::make_shared<rtk::Menu>();
 
-        auto fileNewMenu = std::make_shared<rtk::Menu>();
+        fileMenu->add(L"&New\tCtrl+N", [this] () {});
 
-        fileNewMenu->add(L"New &Text", [this] () {
-        
-        });
+        fileMenu->add(L"&Open\tCtrl+O", [this] () {});
 
-        fileNewMenu->add(L"New &Hex", [this] () {
-        
-        });
+        fileMenu->add(L"&Save\tCtrl+S", [this] () {});
 
-        fileMenu->add(L"&New", fileNewMenu);
+        fileMenu->add(L"Save &As", [this] () {});
 
         fileMenu->add(L"&Exit\tAlt+F4", [this] () {
             close();
@@ -57,6 +53,21 @@ namespace notes
 
         menu->add(L"&File", fileMenu);
 
+        auto editMenu = std::make_shared<rtk::Menu>();
+
+        editMenu->add(L"&Undo\tCtrl+Z", [this] () {});
+
+        editMenu->add(L"&Redo\tCtrl+Y", [this] () {});
+
+        editMenu->add(L"&Options", [this] () {});
+
+        menu->add(L"&Edit", editMenu);
+
+        auto helpMenu = std::make_shared<rtk::Menu>();
+
+        helpMenu->add(L"&About", [this] () {});
+
+        menu->add(L"&Help", helpMenu);
 
         set_menu(menu);
     }
